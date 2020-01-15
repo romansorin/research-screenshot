7436b268c2882c5574d20325ba2074a7a96b2085
Roman Sorin <rmaximsorin@gmail.com>
2020-01-15 12:36:36 -0500

    removed migrate from main

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.session import sessionmaker
-from migrations.Site import Site
-from config.database import conn_string
-from models.Database import Database
-
 if __name__ == "__main__":
-    Database.drop()
-    Database.migrate()
-
-# driver = Driver()
-# for site in sites:
-#     start_time, last_height = setup(site["name"], site["url"])
-#     last_height = scroll(last_height)
-#     rescroll(last_height)
-#     screenshot(site["name"], start_time, last_height)
-# driver.quit()
+    # driver = Driver()
+    # for site in sites:
+    #     start_time, last_height = setup(site["name"], site["url"])
+    #     last_height = scroll(last_height)
+    #     rescroll(last_height)
+    #     screenshot(site["name"], start_time, last_height)
+    # driver.quit()
