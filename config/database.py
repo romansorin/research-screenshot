52df180bab9944d7fe4663e1e9dfb542be603f66
Roman Sorin <rmaximsorin@gmail.com>
2020-01-15 11:52:43 -0500

    moved methods from config into db model

--- config/database.py
+++ config/database.py
@@ -1,29 +1,7 @@
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm.session import sessionmaker
 
 # TODO: Check that correct database is being referenced and working properly
 database_name = 'screenshots'
 database = os.path.join(os.path.dirname('../storage/'), f"{database_name}.sqlite3")
 conn_string = f'sqlite:///{database}'
-
-def _setup():
-    eng = create_engine(conn_string)
-    Session = sessionmaker(bind=eng)
-    return Session(), eng
-
-
-Base = declarative_base()
-session, engine = _setup()
-
-
-def migrate():
-    Base.metadata.create_all(engine)
-    print("Migrated tables")
-
-
-def drop():
-    Base.metadata.drop_all(engine)
-    print("Dropped tables")
--- models/Database.py
+++ models/Database.py
@@ -1,3 +1,23 @@
+
+from config.database import conn_string
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+Base = declarative_base()
+
 class Database:
     def __init__(self):
-        pass
+        eng = create_engine(conn_string)
+        Session = sessionmaker(bind=eng)
+        self.session = Session()
+        self.engine = eng
+
+
+    def migrate(self):
+        Base.metadata.create_all(self.engine)
+        print("Migrated tables")
+
+    def drop(self):
+        Base.metadata.drop_all(self.engine)
+        print("Dropped tables")
