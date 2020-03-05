9fdd33c82e47aada32e64579dfe43c4b68058382
Roman Sorin <roman@romansorin.com>
2020-03-04 23:05:40 -0500

    parse responses and store in db

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from models.Base import Base, Session, engine
-from migrations.Site import Site
-from migrations.Screenshot import Screenshot
-from migrations.Response import Response
-from migrations.ParsedResponse import ParsedResponse
-from config.aws import API_KEY
-import requests
-import time
-import json
 import datetime
+import json
+
+import requests
+
 from config.app import STORAGE_LOGS_PATH, QUERY_START_PATH
+from config.aws import HEADERS
+from migrations.ParsedResponse import ParsedResponse
+from migrations.Response import Response
+from models.Base import Base, Session, engine
 
-a = {'Ats': {'OperationRequest': {'RequestId': '0c7cf045-1e12-4dc0-839d-ea9549bb3c3c'}, 'Results': {'Result': {'Alexa': {'Request': {'Arguments': {'Argument': [{'Name': 'countrycode', 'Value': 'Global'}, {'Name': 'start', 'Value': '1'}, {'Name': 'count', 'Value': '10'}, {'Name': 'responsegroup', 'Value': 'Country'}]}}, 'TopSites': {'Country': {'CountryName': 'GLOBAL', 'CountryCode': '*', 'TotalSites': '104520', 'Sites': {'Site': [{'DataUrl': 'google.com', 'Country': {'Rank': '1', 'Reach': {'PerMillion': '592500'}, 'PageViews': {'PerMillion': '245220', 'PerUser': '14.77'}}, 'Global': {'Rank': '1'}}, {'DataUrl': 'youtube.com', 'Country': {'Rank': '2', 'Reach': {'PerMillion': '324100'}, 'PageViews': {'PerMillion': '64028', 'PerUser': '7.05'}}, 'Global': {'Rank': '2'}}, {'DataUrl': 'tmall.com', 'Country': {'Rank': '3', 'Reach': {'PerMillion': '115400'}, 'PageViews': {'PerMillion': '9215', 'PerUser': '2.85'}}, 'Global': {'Rank': '3'}}, {'DataUrl': 'facebook.com', 'Country': {'Rank': '4', 'Reach': {'PerMillion': '68340'}, 'PageViews': {'PerMillion': '15000', 'PerUser': '7.84'}}, 'Global': {'Rank': '4'}}, {'DataUrl': 'baidu.com', 'Country': {'Rank': '5', 'Reach': {'PerMillion': '77700'}, 'PageViews': {'PerMillion': '9294', 'PerUser': '4.27'}}, 'Global': {'Rank': '5'}}, {'DataUrl': 'qq.com', 'Country': {'Rank': '6', 'Reach': {'PerMillion': '76900'}, 'PageViews': {'PerMillion': '8652', 'PerUser': '4.01'}}, 'Global': {'Rank': '6'}}, {'DataUrl': 'sohu.com', 'Country': {'Rank': '7', 'Reach': {'PerMillion': '71600'}, 'PageViews': {'PerMillion': '9360', 'PerUser': '4.67'}}, 'Global': {'Rank': '7'}}, {'DataUrl': 'login.tmall.com', 'Country': {'Rank': '8', 'Reach': {'PerMillion': '105000'}, 'PageViews': {'PerMillion': '2941', 'PerUser': '1'}}, 'Global': {'Rank': '8'}}, {'DataUrl': 'taobao.com', 'Country': {'Rank': '9', 'Reach': {'PerMillion': '60700'}, 'PageViews': {'PerMillion': '5928', 'PerUser': '3.49'}}, 'Global': {'Rank': '9'}}, {'DataUrl': '360.cn', 'Country': {'Rank': '10', 'Reach': {'PerMillion': '53300'}, 'PageViews': {'PerMillion': '5850', 'PerUser': '3.92'}}, 'Global': {'Rank': '10'}}]}}}}}, 'ResponseStatus': {'StatusCode': '200'}}}}
-
-
-# Start
 
 def migrate_fresh():
     Base.metadata.drop_all(engine)
@@ -23,7 +18,7 @@ def migrate_fresh():
 
 
 """
-1. Query AWS API
+DONE - 1. Query AWS API
  - Add response to database
  - Update start variable (found in plaintext txt)
 
@@ -47,9 +42,8 @@ def parse_response(data):
     return data['Ats']['Results']['Result']['Alexa']['TopSites']['Country']['Sites']['Site']
 
 
-headers = {'x-api-key': API_KEY}
 def make_api_request(url):
-    return requests.get(url, headers=headers)
+    return requests.get(url, headers=HEADERS)
 
 
 def log_response(data):
@@ -57,12 +51,12 @@ def log_response(data):
     timestamp = str(timestamp).replace(" ", "_").replace(".", "-").replace(":", "-")
     filename = f"query_{timestamp}.log"
     f = open(f"{STORAGE_LOGS_PATH}/{filename}", "x")
-    f.write(str(datetime.datetime.now())+ "\n\n")
+    f.write(str(datetime.datetime.now()) + "\n\n")
     f.write(json.dumps(data))
     f.close()
 
 
-if __name__ == "__main__":
+def collect_aws_data():
     session = Session()
     limit = 3
     lower = 1
@@ -86,11 +80,51 @@ if __name__ == "__main__":
 
     session.close()
 
-    # # r = requests.get(alexa_url, headers=headers)
-    # f = open("test.json", "w")
-    # # print(r.json())
-    # # f.write(json.dumps(a))
-    # f.close()
+
+def parse_collected_data():
+    session = Session()
+    data = session.query(Response).filter_by(parsed=False)
+
+    timestamp = datetime.datetime.now()
+    timestamp = str(timestamp).replace(" ", "_").replace(".", "-").replace(":", "-")
+    filename = f"parsed_response_{timestamp}.log"
+    f = open(f"{STORAGE_LOGS_PATH}/{filename}", "x")
+    f.write(str(datetime.datetime.now()) + "\n\n")
+
+    for site in data:
+        response_id = site.id
+        print(f"Beginning response number {response_id}")
+        f.write(f"Beginning response number {response_id} \n")
+
+        for response in parse_response(site.response):
+            url = response['DataUrl']
+            print(f"Beginning site {url}")
+            f.write(f"Beginning site {url}: ")
+            rank = response['Global']['Rank']
+            reach_per_million = response['Country']['Reach']['PerMillion']
+            page_views_per_million = response['Country']['PageViews']['PerMillion']
+            page_views_per_user = response['Country']['PageViews']['PerUser']
+            f.write(
+                f"Rank: {rank}, Reach/Million: {reach_per_million}, Page Views/Million: {page_views_per_million}, Page Views/User: {page_views_per_user} \n")
+            parsed_response = ParsedResponse(response_id=response_id, url=url, rank=rank,
+                                             reach_per_million=reach_per_million,
+                                             page_views_per_million=page_views_per_million,
+                                             page_views_per_user=page_views_per_user)
+            session.add(parsed_response)
+            session.commit()
+
+        session.query(Response).get(response_id).parsed = True
+        print(f"Finished parsing response number {response_id}")
+        f.write(f"Finished parsing response number {response_id} \n\n")
+        session.commit()
+
+    session.close()
+    f.close()
+
+
+
+if __name__ == "__main__":
+    parse_collected_data()
     # driver = Driver()
     # for site in sites:
     #     start_time, last_height = setup(site["name"], site["url"])
@@ -98,5 +132,3 @@ if __name__ == "__main__":
     #     rescroll(last_height)
     #     screenshot(site["name"], start_time, last_height)
     # driver.quit()
-
-
